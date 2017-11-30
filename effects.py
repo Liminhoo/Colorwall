0e0f48d011d727919d582ffbd7b5985a8bbc1685
Liminhoo <31395194+Liminhoo@users.noreply.github.com>
2017-11-30 18:59:13 +0900

    60135143임인후
    임인후 이름의 영문으로 나오도록 처리

--- effects.py
+++ effects.py
@@ -338,7 +338,9 @@ class LetterTest(Effect):
 
         # Display upper and lower case letters. The break between 90 and 97 is
         # for non-letter keyboard characters.
-        for ord in range(65, 91) + range(97, 123):
+        new_list = [76, 105, 109, 73, 110, 72, 111, 111]
+        for ord in new_list :
+        # for ord in range(65, 91) + range(97, 123):
             self.wall.clear()
 
             # Set every pixel to the background color, since ascii8x8 will only
